47a0eb42557a2c748e53536868381ae3e82dc701
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-02-03 01:35:23 +0900

    0202itemを横並びに

---
+++ src/components/item.js
@@ -0,0 +1,6 @@
+import styles from '@/styles/item.module.css'
+export default function Item(){
+    return(
+        <div className="itemList">Item</ div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import Hero from '@/components/hero';
+import Item from '@/components/item';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,9 +17,12 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div>
-      <Header />
       <Hero />
-      <Footer />
+      <ul className="itemsList">
+      <li><Item /></li>
+      <li><Item /></li>
+      <li><Item /></li>
+      </ ul>
 
 
     </div>
