f41ae06467e1c2cd934aec189ea5c00aa33fe32b
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-02-02 18:39:07 +0900

    0202ファイルをtsxからjsに変更（できるんだ......）

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>OH!Kirby-chan.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
