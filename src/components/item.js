868ea54e350c3e2015b36b29337a7d2b4ba17067
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-02-03 02:26:00 +0900

    0203itemboxのスタイル

--- src/components/item.js
+++ src/components/item.js
@@ -1,6 +1,21 @@
 import styles from '@/styles/item.module.css'
 export default function Item(){
     return(
-        <div className="itemList">Item</ div>
+        <div className={styles.box}>
+            <div className={styles.icon}>
+                アイコン
+            </div>
+            <div className={styles.textbox}>
+            <hh3 className={styles.title}>
+                タイトル
+            </hh3>
+            <div>
+            <hh4 className={styles.exp}>
+                本文あああああああああああ
+            </hh4>
+            </div>
+            </div>
+
+        </ div>
     )
 }
