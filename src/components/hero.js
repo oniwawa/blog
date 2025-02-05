744e5cbb1ea4e0f3eaf915f1072cb68f949ac9be
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-02-05 23:43:38 +0900

    0204ヒーローヘッダーいれ

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,22 @@
+import Image from 'next/image'
+import styles from '@/styles/hero.module.css'
+import image from  '@/components/images/div.right.png'
 export default function Hero(){
     return(
-        <footer>CUBE</footer>
+        <div className="itemsList">
+            <div className={styles.titleTextbox}>
+                <hh3>生成AI活用基盤構築 ~End-To-End Support~</hh3>
+                <hh1 className={styles.titleBold}>生成AIベースの</hh1>
+                <hh1>Azureインフラ構築</hh1>
+            </div>
+            <div className={styles.titleImage}>
+                <Image
+                src={image}
+                alt="トップ画像"
+                layout="responsive"
+                />
+            </div>
+
+        </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from '@/components/header';
-import Footer from '@/components/footer';
 import Hero from '@/components/hero';
 import Item from '@/components/item';
 const geistSans = Geist({
@@ -17,12 +15,12 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Hero className="pb-24"/>
       <ul className="itemsList">
       <li><Item /></li>
       <li><Item /></li>
       <li><Item /></li>
-      </ ul>
+      </ul>
 
 
     </div>
